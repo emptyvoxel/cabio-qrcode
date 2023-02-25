7cf1ea8c06f28039f4a27c7f10a8ab994652a368
Gabriel <89980949+emptyvoxel@users.noreply.github.com>
2023-02-24 21:19:19 -0300

    Minor changes

--- src/components/input/Select.tsx
+++ src/components/input/Select.tsx
@@ -6,10 +6,11 @@ import { Options } from 'qr-code-styling';
 const Select: React.FC<{
     className?: string;
     opts: string[];
-    dataKey: 'cornersSquareOptions' | 'cornersDotOptions' | 'dotsOptions' | 'backgroundOptions';
+    dataKey: 'cornersSquareOptions' | 'cornersDotOptions' | 'dotsOptions';
     options: Options;
     setOptions: React.Dispatch<React.SetStateAction<Options>>;
 }> = ({ className, opts, dataKey, options, setOptions }) => {
+    const isActive = (opt: string) => options[dataKey]?.type === opt;
     const onClick = (opt: string) => {
         return setOptions({
             ...options,
@@ -23,7 +24,9 @@ const Select: React.FC<{
     return (
         <div className={className}>
             {opts.map(opt => (
-                <div onClick={() => onClick(opt)}>
+                <div
+                    title={opt}
+                    onClick={() => onClick(opt)} className={isActive(opt) ? 'active' : ''}>
                     <Icon>{`${dataKey}-${opt}`}</Icon>
                 </div>
             ))}
@@ -32,7 +35,16 @@ const Select: React.FC<{
 }
 
 export default styled(Select)`
+    display: flex;
+    align-items: center;
+
     > div {
-        display: inline-block;
+        cursor: pointer;
+        display: flex;
+        margin: 0 5px;
+
+        &.active svg {
+            fill: #f0f;
+        }
     }
 `;
